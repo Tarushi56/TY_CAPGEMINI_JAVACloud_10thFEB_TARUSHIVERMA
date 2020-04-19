44a15a43182efbea3e0124e0fb93682d6afc86cb
Tarushi56 <tarushiverma6159@gmail.com>
2020-04-19 17:46:17 +0530

    spring assignment

---
+++ springmvc/src/main/java/com/tyss/springmvc/service/EmployeeService.java
@@ -0,0 +1,22 @@
+package com.tyss.springmvc.service;
+
+import java.util.List;
+
+import com.tyss.springmvc.beans.EmployeeDetails;
+import com.tyss.springmvc.beans.Login;
+
+public interface EmployeeService {
+
+
+	Boolean validate(Login user);
+
+	void add(EmployeeDetails employeeDetails);
+
+	List<EmployeeDetails> list();
+
+	void delete(Integer id);
+
+	void edit(EmployeeDetails employeeDetails);
+
+	EmployeeDetails get(Integer id);	
+}
